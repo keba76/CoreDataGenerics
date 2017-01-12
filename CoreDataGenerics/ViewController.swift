63e23d6cbfc2a463d8d8a31f043cdacd11d81bfa
keba76 <Harman@macbook-air-ievgen.local>
2017-01-12 07:55:44 +0200

    Initial Commit

---
+++ CoreDataGenerics/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreDataGenerics
+//
+//  Created by Ievgen Keba on 1/12/17.
+//  Copyright © 2017 Harman Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
